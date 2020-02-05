13d336dea6e69068c4eb932530481cf23509c414
Nikolay Kanatov <nikolay.kanatov@firstlinesoftware.com>
2020-02-05 13:32:07 +0300

    added infrastructure template project

---
+++ Infrastructure/DataAccess/ISession.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace Infrastructure.DataAccess
+{
+    public interface ISession
+    {
+        IEnumerable<T> Query<T>(string sql, object param = null, bool buffered = true, CommandType? commandType = null, int? commandTimeout = null);
+
+        Task<IEnumerable<T>> QueryAsync<T>(string sql, object param = null, bool buffered = true, CommandType? commandType = null, int? commandTimeout = null);
+
+        int Execute(string sql, object param = null, CommandType? commandType = null, int? commandTimeout = null);
+
+        Task<int> ExecuteAsync(string sql, object param = null, int? commandTimeout = null, CommandType? commandType = null);
+
+        Task<T> ExecuteScalarAsync<T>(string sql, object param, int? commandTimeout = null, CommandType? commandType = null);
+
+        T ExecuteScalar<T>(string sql, object param, int? commandTimeout = null, CommandType? commandType = null);
+
+        void Commit();
+
+        void Rollback();
+    }
+}
---
+++ Infrastructure/DataAccess/ISessionFactory.cs
@@ -0,0 +1,9 @@
+﻿using System.Data;
+
+namespace Infrastructure.DataAccess
+{
+    public interface ISessionFactory
+    {
+        ISession CreateForEmployee(bool createTransaction = false, IsolationLevel isolationLevel = IsolationLevel.ReadUncommitted);
+    }
+}
