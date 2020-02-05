dcc0cf6d034efdfdcfe6d52cad0ae94a9a74b6eb
EkaterinaKuznetsova9 <32989353+EkaterinaKuznetsova9@users.noreply.github.com>
2020-02-05 14:30:08 +0300

    added biz rules template project

---
+++ Department/IDepartmentBizRules.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Domain.Models;
+
+namespace BizRules.Department
+{
+    public interface IDepartmentBizRules
+    {
+        Task<IEnumerable<DictionaryItem>> GetAll();
+    }
+}
