4043d2bdf8289d2dc0e186fc632fb28b97ee5e24
Nikolay Kanatov <nikolay.kanatov@firstlinesoftware.com>
2020-02-12 08:25:55 +0300

    added service template for rmq service

---
+++ RmqSubscriber/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.ServiceProcess;
+
+namespace RmqSubscriber
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new Service1()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
